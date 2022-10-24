47f5b2875f9c826f419e1283262e5fa7f72988aa
MykeDuong <hongminh4402@gmail.com>
2022-10-24 15:48:49 -0700

    Nav Bar

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  experimental: {
+    images: {
+        allowFutureImage: true
+    }
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
 }
 
 module.exports = nextConfig
