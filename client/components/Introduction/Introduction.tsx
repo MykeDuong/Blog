26cfdf7682e64c5d53af029e7c658bb6078b2b61
Minh Duong <hongminh4402@gmail.com>
2022-11-17 20:53:43 -0700

    Debugging

--- client/components/Introduction/Introduction.tsx
+++ client/components/Introduction/Introduction.tsx
@@ -6,6 +6,9 @@ import styles from './Introduction.module.scss';
 import Image from 'next/future/image';
 import Link from 'next/link';
 
+import globalStyles from '../../styles/Home.module.scss';
+import cssVar from '../../styles/variables.module.scss';
+
 interface Props {
   author: AuthorInterface;
 }
@@ -19,7 +22,7 @@ const Introduction: NextComponentType<{}, {}, Props> = ({ author }) => {
       <div className={styles.app__introductionTextBox}>
         <h1 className={styles.welcome}><span>👋🏻</span>  Hello! And welcome to my blog!</h1>
         <p className={styles.introductionText}>I am Minh, a Computer Science & Economics student at the University of Arizona</p>
-        <p className={styles.introductionText}><Link href="/portfolio">Click here</Link> to see my portfolio. Otherwise, check out my blogs down below! Thank you for coming by!</p>
+        <p className={styles.introductionText}><Link href="/portfolio"><a className={globalStyles.app__anchor} style={{ color: cssVar.blackColor }}>Click here</a></Link> to see my portfolio. Otherwise, check out my blogs down below! Thank you for coming by!</p>
       </div>
     </div>
   )
