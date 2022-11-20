a43571c9abd96a9f02a067834cb33be0d094dab3
Minh Duong <hongminh4402@gmail.com>
2022-11-20 00:02:20 -0700

    Theme Tag

--- client/pages/tags/index.tsx
+++ client/pages/tags/index.tsx
@@ -9,8 +9,10 @@ import client from '../../client';
 import TagInterface from '../../interfaces/TagInterface';
 import Button from '../../components/Button/Button';
 import Link from 'next/link';
+import useStore from '../../store';
 
 const Tags: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({ tags }) => {
+  const { theme } = useStore();
   return (
     <div className={globalStyles.app} >
       <Header title="Tags" subtitle="Right now, what I know" mainPage={true} mainImage="tagIMG" />
@@ -20,13 +22,12 @@ const Tags: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({ tags }
         </div>
         <div className={styles.app__tagContents}>
           {tags.map((tag: TagInterface) => 
-          <div key={`content of ${tag.title}`} className={styles.app__tagContent} id={tag.title}>
-            <div className={styles.tagTitle} ><AiFillTag /><p>{tag.title}</p></div>
+          <div key={`content of ${tag.title}`} className={`${styles.app__tagContent} ${theme ? styles.app__tagContentLight : styles.app__tagContentDark}`} id={tag.title}>
+            <div className={`${styles.tagTitle} ${theme ? styles.tagTitleLight : styles.tagTitleDark}`} ><AiFillTag /><p>{tag.title}</p></div>
             <div className={styles.app__tagPosts}>
-              {tag.posts.map(post =>
-              <p key={`post of ${post.title}`} className={styles.postTitle} >
-                <Link href={`blogs/${post.slug.current}`}>{post.title}</Link>
-              </p>)}
+              {tag.posts.map(post => (
+                <Link key={`link of ${post.title}`} href={`blogs/${post.slug.current}`}><a className={`${styles.postTitle} ${theme ? styles.postTitleLight : styles.postTitleDark}`}>{post.title}</a></Link>
+              ))}
             </div>
           </div>)}
         </div>
